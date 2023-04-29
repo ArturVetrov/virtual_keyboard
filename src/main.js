fcca839794352449befd6df6ee1c06b0174a8261
Artur <artur97s@yandex.ru>
2023-04-29 21:04:39 +0300

    feat: add SelectLanguage function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /* const output = document.querySelector('.output-text'); */
 const CaseUp = document.querySelectorAll('.case-up');
 const CaseDown = document.querySelectorAll('.case-down');
+const RuKeyboard = document.querySelectorAll('.ru');
+const EngKeyboard = document.querySelectorAll('.eng');
 
 function UpperCase(event) {
   const isCaps = event.getModifierState('CapsLock');
@@ -17,16 +19,34 @@ function UpperCase(event) {
   }
 }
 
+function SelectLanguage(event) {
+  if (event.ctrlKey && event.code === 'MetaLeft') {
+    if (!RuKeyboard[0].classList.contains('hidden')) {
+      for (let i = 0; i < RuKeyboard.length; i += 1) {
+        RuKeyboard[i].classList.add(('hidden'));
+        EngKeyboard[i].classList.remove(('hidden'));
+      }
+    } else {
+      for (let i = 0; i < RuKeyboard.length; i += 1) {
+        RuKeyboard[i].classList.remove(('hidden'));
+        EngKeyboard[i].classList.add(('hidden'));
+      }
+    }
+  }
+}
+
 document.addEventListener('keydown', (event) => {
-  /* console.log(event.code); */
+  /* console.log(event);
+  console.log(event.code); */
   const button = document.querySelector(`#${event.code}`);
   button.parentNode.parentNode.classList.add('active');
   UpperCase(event);
+  SelectLanguage(event);
 });
 
 document.addEventListener('keyup', (event) => {
   /* console.log(event.code); */
-  console.log(event);
+  /* console.log(event); */
   const button = document.querySelector(`#${event.code}`);
   button.parentNode.parentNode.classList.remove('active');
   UpperCase(event);
