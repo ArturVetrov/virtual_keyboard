f3a2b7d619b5d8a0d7ec9c1de8c0751e2afe6dfc
Artur <artur97s@yandex.ru>
2023-04-30 18:23:49 +0300

    feat: add CapsLock function

--- src/main.js
+++ src/main.js
@@ -1,19 +1,35 @@
-/* const output = document.querySelector('.output-text'); */
+const Output = document.querySelector('.output-text');
 const CaseUp = document.querySelectorAll('.case-up');
 const CaseDown = document.querySelectorAll('.case-down');
+const CapsLock = document.querySelectorAll('.caps-lock');
 const RuKeyboard = document.querySelectorAll('.ru');
 const EngKeyboard = document.querySelectorAll('.eng');
 
-function UpperCase(event) {
-  const isCaps = event.getModifierState('CapsLock');
-  if (event.shiftKey || isCaps) {
+function ShiftKey(event) {
+  if (event.shiftKey) {
     for (let i = 0; i < CaseUp.length; i += 1) {
       CaseUp[i].classList.remove('hidden');
       CaseDown[i].classList.add('hidden');
+      CapsLock[i].classList.add('hidden');
     }
-  } else if (!event.shiftKey || !isCaps) {
+  } else if (!event.shiftKey) {
     for (let i = 0; i < CaseUp.length; i += 1) {
       CaseUp[i].classList.add('hidden');
+      CaseDown[i].classList.remove('hidden');
+    }
+  }
+}
+
+function CapsKey(event) {
+  const isCaps = event.getModifierState('CapsLock');
+  if (isCaps && event.code === 'CapsLock') {
+    for (let i = 0; i < CaseUp.length; i += 1) {
+      CapsLock[i].classList.remove('hidden');
+      CaseDown[i].classList.add('hidden');
+    }
+  } else if (!isCaps && event.code === 'CapsLock') {
+    for (let i = 0; i < CaseUp.length; i += 1) {
+      CapsLock[i].classList.add('hidden');
       CaseDown[i].classList.remove('hidden');
     }
   }
@@ -40,8 +56,9 @@ document.addEventListener('keydown', (event) => {
   console.log(event.code); */
   const button = document.querySelector(`#${event.code}`);
   button.parentNode.parentNode.classList.add('active');
-  UpperCase(event);
+  ShiftKey(event);
   SelectLanguage(event);
+  CapsKey(event);
 });
 
 document.addEventListener('keyup', (event) => {
@@ -49,5 +66,6 @@ document.addEventListener('keyup', (event) => {
   /* console.log(event); */
   const button = document.querySelector(`#${event.code}`);
   button.parentNode.parentNode.classList.remove('active');
-  UpperCase(event);
+  ShiftKey(event);
+  CapsKey(event);
 });
